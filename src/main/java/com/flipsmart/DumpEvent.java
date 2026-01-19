cb58817ea7aa6b99b5a21f3445fc865c3aff49b1
brettkrueger <admin@krux.us>
2026-01-19 15:09:04 -0600

    Ensure DumpEvent doesn't abbreviate GP

--- src/main/java/com/flipsmart/DumpEvent.java
+++ src/main/java/com/flipsmart/DumpEvent.java
@@ -130,25 +130,10 @@ public class DumpEvent
 	}
 
 	/**
-	 * Format GP with K/M/B suffixes
+	 * Format GP with commas
 	 */
 	private static String formatGP(int value)
 	{
-		if (value >= 1_000_000_000)
-		{
-			return String.format("%.1fB", value / 1_000_000_000.0);
-		}
-		else if (value >= 1_000_000)
-		{
-			return String.format("%.1fM", value / 1_000_000.0);
-		}
-		else if (value >= 1_000)
-		{
-			return String.format("%.1fK", value / 1_000.0);
-		}
-		else
-		{
-			return String.format("%dgp", value);
-		}
+		return String.format("%,d", value);
 	}
 }
